7694e43135182b7cac68e8ec274830d41dac3c12
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-06 22:35:25 -0400

    change data source

--- build_keras_sequential_model.py
+++ build_keras_sequential_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a054dbcf53aad3b9bc3b01dad35d6e5306462827f160af667cbf2f944ac798c
-size 2681
+oid sha256:3bc92b269600bc3174e5a6dbe6fe118eaccfff394a481b1e4f2d06f9a6996145
+size 2877
