ad73b6f0c588de430953ce897ecf1ad4df4d516e
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-07 22:43:31 -0400

    show correct prediction probability

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3557d88e2574b1554efd4ca00029de2c5e463ec163db33d022d9dc1c96ac611
-size 5114
+oid sha256:a49f2c3e2ce805a9879cceb9bdf62793c0cb0bb6186616c83da9b253d5a132a0
+size 4830
