87eeeeadeaa2b781fda2ef3dbe0345d771bd9afa
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-07 23:00:44 -0400

    update preprocessing steps

--- preprocessing_image.py
+++ preprocessing_image.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:345836a5ff4f1597298defa7bc0e94abb5bdf73fd8f4c6b8a88c2b22f6e01872
-size 2342
+oid sha256:5c4bbc1091fe220f03dbc9df4d88c03c5e0291cdd55c7464d7c4ca478b338f1b
+size 2396
