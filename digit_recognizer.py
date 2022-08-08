aa040fad0ab482532b39921d90d577cabaf442d7
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-07 23:45:49 -0400

    add option to display probabilities

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4ad6e71f3f5003e73636ef6e1ecd6d96c3ff12a43c09b1e7822607c88d1c0d4
-size 5523
+oid sha256:1d03ef64914c2aa726cf8e5bb449adbbf2449cefe53a93901ba672349dbab0ca
+size 6867
