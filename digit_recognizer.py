3557fddcd568aade12e10e111d59a8fc6d2dbf69
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-07 22:48:36 -0400

    adjust app window

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a49f2c3e2ce805a9879cceb9bdf62793c0cb0bb6186616c83da9b253d5a132a0
-size 4830
+oid sha256:b733cfdb133a33c1f996fc3a5c3f18ca87ce1ae1369160f47876dc7df9926ad3
+size 4925
