e66b35edb95805e0d9f87e64ffc0898ccf9daa21
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-08 12:24:20 -0400

    remove git lfs

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4d541d679d1aad7251bdb64478fe39d3db80fa978af0696daa62165d807a9c64
-size 807
+# MNIST Handwritten Digit Classification
+* Classify a handwritten digit!
+* Run `build_model.py` to build the machine learning model that is trained and tested using the MNIST dataset
+* Run `digit_recognizer.py` to open the app to classify your own handwritten digit
+* Based on the project ["Handwritten Digit Recognition using Python"](https://data-flair.training/blogs/python-deep-learning-project-handwritten-digit-recognition/) with some adaptions
+    * Major Improvements:
+    * Used the Random Forest Classifier to avoid overfitting (https://towardsdatascience.com/improving-accuracy-on-mnist-using-data-augmentation-b5c38eb5a903?gi=716f038b89fc)
+    * Preprocessed images so that classifier has a higher accuracy rate (https://opensourc.es/blog/tensorflow-mnist/)
+    * Code structure and readability
--- preprocessing_image.py
+++ preprocessing_image.py
@@ -1,3 +1,83 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5c4bbc1091fe220f03dbc9df4d88c03c5e0291cdd55c7464d7c4ca478b338f1b
-size 2396
+#---------Imports
+from sklearn.datasets import fetch_openml
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import cv2
+import math
+from scipy import ndimage
+#---------End of imports
+
+### FUNCTIONS ###
+def get_data():
+    ''' load the MNIST dataset '''
+
+    mnist = fetch_openml('mnist_784', version=1) # load data
+    data, labels = mnist['data'], mnist['target'] # get data and labels
+    print('Sucessfully loaded data')
+
+    return (data[:60000], data[60000:], labels[:60000], labels[60000:])
+
+def getBestShift(img):
+    cy,cx = ndimage.measurements.center_of_mass(img)
+
+    rows,cols = img.shape
+    shiftx = np.round(cols/2.0-cx).astype(int)
+    shifty = np.round(rows/2.0-cy).astype(int)
+
+    return shiftx,shifty
+
+def shift(img,sx,sy):
+    rows,cols = img.shape
+    M = np.float32([[1,0,sx],[0,1,sy]])
+    shifted = cv2.warpAffine(img,M,(cols,rows))
+    return shifted
+
+### MAIN FLOW ###
+if __name__ == '__main__':
+    data_train, data_test, labels_train, labels_test = get_data() # get mnist data
+
+    clf = RandomForestClassifier(random_state=42) # initialize classifier
+    clf.fit(data_train, labels_train) # train classifier
+    print('Training Random Forest Classifier ...')
+
+    # preprocess image
+    data = np.load('new_image.npz')
+    gray = data['img']
+
+    while np.sum(gray[0]) == 0:
+        gray = gray[1:]
+
+    while np.sum(gray[:,0]) == 0:
+        gray = np.delete(gray,0,1)
+
+    while np.sum(gray[-1]) == 0:
+        gray = gray[:-1]
+
+    while np.sum(gray[:,-1]) == 0:
+        gray = np.delete(gray,-1,1)
+
+    rows,cols = gray.shape
+
+    # resize to be contained within 20 x 20 box
+    if rows > cols:
+        factor = 20.0/rows
+        rows = 20
+        cols = int(round(cols*factor))
+        gray = cv2.resize(gray, (cols,rows))
+    else:
+        factor = 20.0/cols
+        cols = 20
+        rows = int(round(rows*factor))
+        gray = cv2.resize(gray, (cols, rows))
+
+    # pad to maek 28 x 28 image
+    colsPadding = (int(math.ceil((28-cols)/2.0)),int(math.floor((28-cols)/2.0)))
+    rowsPadding = (int(math.ceil((28-rows)/2.0)),int(math.floor((28-rows)/2.0)))
+    gray = np.lib.pad(gray,(rowsPadding,colsPadding),'constant')
+
+    # center number using center of mass
+    shiftx,shifty = getBestShift(gray)
+    shifted = shift(gray,shiftx,shifty)
+    gray = shifted
+
+    data = gray.flatten().transpose().reshape(1, -1)
