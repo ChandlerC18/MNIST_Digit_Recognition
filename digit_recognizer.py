880cf36cf5bc47286bc58801cfc1f9fbdada9e02
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-07 22:28:47 -0400

    fix image_draw not clearing

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34f7822af66c30823a70db1a14b353d1ea79d930149fbaa04c4c7f80d0502ed4
-size 4991
+oid sha256:b3557d88e2574b1554efd4ca00029de2c5e463ec163db33d022d9dc1c96ac611
+size 5114
